e38c91c26cc38c65541ebde69b1e3a612eaf4e48
Sloan Stinnett <Sloan.C.Stinnett-1@ou.edu>
2020-12-17 18:38:51 -0600

    updating ANOVA

--- ANOVA.py
+++ ANOVA.py
@@ -3,19 +3,20 @@ import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import matplotlib.pyplot as mp # plotting 
 import statsmodels.formula.api as sm 
 import statsmodels.stats as sms #ANOVA model
+import statsmodels.api as sma #api entry 
 
 # Creating the dataframe for the houseing data
 Housedb = pd.read_csv('train.csv')
 #looking at the first few rows
 Housedb.head()
 
-#Gather
-Deeds = Housedb.loc[Housedb['SaleType'].isin(['New','WD'])]
+#variable selection 
+Deeds = Housedb.loc[Housedb['SaleType'].isin(['New','WD']) & Housedb['KitchenQual'].isin(['Ex','Gd'])]
 print(Deeds.shape)
 Deeds['SaleType'].value_counts()
 Deeds['KitchenQual'].value_counts()
 print(Deeds)
 
-House_lm = sm.ols(formula="SalePrice ~ C(SaleType)+C(Fence)",data=Deeds).fit()
-table = sms.anova.anova_lm(House_lm,typ=1)
+House_lm = sm.ols(formula="SalePrice ~ C(SaleType)+C(KitchenQual)",data=Deeds).fit()
+table = sma.stats.anova_lm(House_lm,typ=1)
 print(table)
