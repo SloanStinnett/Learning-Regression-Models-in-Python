50f1914c3e76a5bd52919f5845d6e100e21c8e18
Sloan Stinnett <Sloan.C.Stinnett-1@ou.edu>
2020-12-18 19:05:36 -0600

    finalized ANOVA

--- ANOVA.py
+++ ANOVA.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as mp # plotting
 import statsmodels.formula.api as sm 
 import statsmodels.stats as sms #ANOVA model
 import statsmodels.api as sma #api entry 
+import statsmodels.stats.multicomp import pairwise_tukeysd
 
 # Creating the dataframe for the houseing data
 Housedb = pd.read_csv('train.csv')
@@ -17,6 +18,14 @@ Deeds['SaleType'].value_counts()
 Deeds['KitchenQual'].value_counts()
 print(Deeds)
 
+#runnning the factorial ANOVA 
 House_lm = sm.ols(formula="SalePrice ~ C(SaleType)+C(KitchenQual)+C(SaleType):C(KitchenQual)",data=Deeds).fit()
-table = sma.stats.anova_lm(House_lm,typ=1)
+table = sma.stats.anova_lm(House_lm,typ=2)
 print(table)
+
+# running a tukey test on the data to measure the main effects of kitchen quality and sale type
+SaleTypeEta = table['sum_sq'][0]/table['sum_sq'][3]
+print(SaleTypeEta)
+
+KitQualEta = table['sum_sq'][1]/table['sum_sq'][3]
+print(KitQualEta)
